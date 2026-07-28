00eafaf521117d326a4fcbe2f6ed43a078add371
dimix <299950422+Dimixixixiixix@users.noreply.github.com>
2026-07-28 12:36:59 -0700

    Uhh not sure but it should work now…
    When i make the pull request delete the base line

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/bartcode-run-event-block-/',
   server: {
     port: 9000
   }
