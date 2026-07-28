879c965c45d6c8b40b43a61bd2037ae70e994daf
dimix <299950422+Dimixixixiixix@users.noreply.github.com>
2026-07-28 12:40:37 -0700

    Ok that didnt work
    I will make the preview in vercel then

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/bartcode-run-event-block-/',
   server: {
     port: 9000
   }
